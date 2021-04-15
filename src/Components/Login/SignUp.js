37fc5355d5323dbf7d7f6c16a27be2f627bb866a
Patrick Lardner <16062927+Okervill@users.noreply.github.com>
2021-04-15 01:07:55 +0100

    updated token method

--- src/Components/Login/SignUp.js
+++ src/Components/Login/SignUp.js
@@ -42,24 +42,22 @@ const SignUp = () => {
       first_name: userdata.additionalUserInfo.profile.given_name,
       last_name: userdata.additionalUserInfo.profile.family_name,
     }
-    console.log(auth.currentUser.getIdToken())
-    console.log(auth.currentUser.getIdTokenResult())
-    //This is commented out pending response in discord from Javi
-    //postNewUser(newUserData)
+
+    postNewUser(newUserData)
   }
 
   const postNewUser = async (postBody) => {
-    const authtoken = await auth.currentUser.getIdTokenResult()
-    const options = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json', 'authorization': authtoken.token },
-      body: JSON.stringify({
-        postBody
-      })
-    };
-    const response = await fetch('https://develop-dot-ip3-online-teaching-platform.appspot.com/student/post', options);
-    const data = await response.json();
-    console.log(data)
+    const body = JSON.stringify(postBody)
+    auth.currentUser.getIdToken(true).then(async (authtoken) => {
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'Authorization': authtoken },
+        body: body
+      };
+      const response = await fetch('https://develop-dot-ip3-online-teaching-platform.appspot.com/student/post', options);
+      const data = await response.json();
+      console.log(data)
+    }).catch(err => console.log(err));
   };
 
   const onChangeHandler = event => {
@@ -76,21 +74,21 @@ const SignUp = () => {
   };
 
   return (
-    <div class="register-container">
-      <div class="main-login-container">
-        <div class="google-facebook-container">
-          <div class="google-facebook">
-            <div class="google-facebook-button">
-              <div class="image-container">
-                <img src="/assets/googlelogo.png" class="google-button-image" alt='' />
+    <div className="register-container">
+      <div className="main-login-container">
+        <div className="google-facebook-container">
+          <div className="google-facebook">
+            <div className="google-facebook-button">
+              <div className="image-container">
+                <img src="/assets/googlelogo.png" className="google-button-image" alt='' />
               </div>
-              <button type="submit" class="google-facebook-link" onClick={(event) => { createUserWithGoogleHandler(event) }}>Sign up with Google</button>
+              <button type="submit" className="google-facebook-link" onClick={(event) => { createUserWithGoogleHandler(event) }}>Sign up with Google</button>
             </div>
-            <div class="google-facebook-button">
-              <div class="image-container">
-                <img src="/assets/facebooklogo.jpg" class="google-button-image" alt='' />
+            <div className="google-facebook-button">
+              <div className="image-container">
+                <img src="/assets/facebooklogo.jpg" className="google-button-image" alt='' />
               </div>
-              <a href="/facebookregister" class="google-facebook-link">
+              <a href="/facebookregister" className="google-facebook-link">
                 Sign up with Facebook
                     </a>
             </div>
@@ -98,20 +96,20 @@ const SignUp = () => {
         </div>
 
 
-        <form class="login-form" action="/dashboard">
-          <p class="login-title">Start learning!</p>
+        <form className="login-form" action="/dashboard">
+          <p className="login-title">Start learning!</p>
 
-          <label class="form-label" for="fname">First name</label>
-          <input type="text" class="form-input" name="fname" value={firstname} onChange={(event) => onChangeHandler(event)} />
+          <label className="form-label" htmlFor="fname">First name</label>
+          <input type="text" className="form-input" name="fname" value={firstname} onChange={(event) => onChangeHandler(event)} />
 
-          <label class="form-label" for="lname">Last name</label>
-          <input type="text" class="form-input" name="lname" value={lastname} onChange={(event) => onChangeHandler(event)} />
+          <label className="form-label" htmlFor="lname">Last name</label>
+          <input type="text" className="form-input" name="lname" value={lastname} onChange={(event) => onChangeHandler(event)} />
 
-          <label class="form-label" for="email">Email</label>
-          <input type="email" class="form-input" name="email" value={email} onChange={(event) => onChangeHandler(event)} />
+          <label className="form-label" htmlFor="email">Email</label>
+          <input type="email" className="form-input" name="email" value={email} onChange={(event) => onChangeHandler(event)} />
 
-          <label class="form-label" for="password">Password</label>
-          <input type="password" class="form-input" name="password" value={password} onChange={(event) => onChangeHandler(event)} />
+          <label className="form-label" htmlFor="password">Password</label>
+          <input type="password" className="form-input" name="password" value={password} onChange={(event) => onChangeHandler(event)} />
 
           {error !== null && (
             <div>
@@ -119,11 +117,11 @@ const SignUp = () => {
             </div>
           )}
 
-          <button type="submit" class="login-form-submit-button" onClick={event => {
+          <button type="submit" className="login-form-submit-button" onClick={event => {
             createUserWithEmailAndPasswordHandler(event, email, password);
           }}>Create account</button>
 
-          <a class="login-form-text" href="/login">Already have an account?</a>
+          <a className="login-form-text" href="/login">Already have an account?</a>
         </form>
 
       </div>
